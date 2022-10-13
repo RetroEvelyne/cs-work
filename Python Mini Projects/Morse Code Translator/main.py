75cc4c2a5153b08eb07b99a1d94cd15a6f4933fb
Evelyne <retroevelyne@outlook.com>
2022-10-13 10:31:17 +0100

    2022-10-13

--- Python Mini Projects/Morse Code Translator/main.py
+++ Python Mini Projects/Morse Code Translator/main.py
@@ -12,25 +12,63 @@ class MorseConverter:
     def __init__(self, file_name: str):
         self.json_file = load_json(file_name)
 
-    def validate_input(self):
-        user_input = input(f"")
+    def get_input(self, input_type: str) -> str | list:
+        valid = False
+        user_input = str
+        while not valid:
+            if input_type == "decoded":
+                user_input = input(f"\nHello, Please Enter Your String!\n{c.purple}╰-> {c.norm}").lower()
+                valid = self.validate_input("decoded", user_input)
+            if input_type == "coded":
+                user_input = input(f"\nHello, Please Enter Your Morse Code!\n"
+                                   f"{c.purple}|{c.cyan} - {c.norm}Each Word Seperated By A '/'\n"
+                                   f"{c.purple}|{c.cyan} - {c.norm}Each Letter Seperated By A Space\n"
+                                   f"{c.purple}╰-> {c.norm}").split()
+                valid = self.validate_input("coded", user_input)
+        return user_input
 
-    def uncoded_to_morse(self):
-        ...
+    def validate_input(self, input_type: str, user_input: str | list) -> bool:
+        if input_type == "decoded":
+            for char in user_input:
+                if char not in self.json_file:
+                    print(f"{c.red}Error, Cannot Translate{c.norm}")
+                    return False
+            return True
 
-    def morse_to_uncoded(self):
-        ...
+        elif input_type == "coded":
+            for char in user_input:
+                if char not in self.json_file.values():
+                    print(f"{c.red}Error, Cannot Translate{c.norm}")
+                    return False
+            return True
+
+    def decoded_to_morse(self):
+        user_input = self.get_input("decoded")
+        final_string = ""
+        for char in user_input:
+            final_string = final_string + str(self.json_file[char]) + " "
+        return final_string
+
+    def morse_to_decoded(self) -> str:
+        user_input = self.get_input("coded")
+        final_string = ""
+        for char in user_input:
+            final_string = final_string + str(list(self.json_file.keys())
+                                              [list(self.json_file.values()).index(char)])
+        return final_string
 
 
 def main(code_json: str):
-    choice = input(f"Do You Want To {c.red}Code {c.norm}Or {c.red}Uncode{c.norm}?\n{c.purple}╰-> {c.norm}").lower()
+    choice = input(f"Do You Want To {c.red}Code {c.norm}Or {c.red}Decode{c.norm}?\n{c.purple}╰-> {c.norm}").lower()
     match choice:
         case "code":
             code = MorseConverter(code_json)
-            code.uncoded_to_morse()
-        case "uncode":
-            uncode = MorseConverter(code_json)
-            uncode.morse_to_uncoded()
+            morse = code.decoded_to_morse()
+            print(f"Your Coded String Is:\n{morse}")
+        case "decode":
+            decode = MorseConverter(code_json)
+            text = decode.morse_to_decoded()
+            print(f"Your Decoded String Is:\n{text}")
         case _:
             print(f"{c.red}Uh Oh... That's Not Right.\n{c.norm}")
             main(code_json)

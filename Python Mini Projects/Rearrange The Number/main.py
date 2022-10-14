81dfc75b17bc7d87f01e9d4461fdc54845a31f58
Replit user <>
2022-10-14 10:29:27 +0000

    repl commit

--- Python Mini Projects/Rearrange The Number/main.py
+++ Python Mini Projects/Rearrange The Number/main.py
@@ -1,9 +1,14 @@
-def reverse():
-    ...
+def reversestring(user_input: str) -> str:
+  final = user_input[::-1]
+  return final
 
 
 def validate_input(user_input: str) -> bool:
-    ...
+    try:
+      int(user_input)
+      valid = True
+    except ValueError:
+      valid = False
     return valid
 
 
@@ -11,13 +16,16 @@ def get_input() -> str:
     valid = False
     user_input = ""
     while not valid:
-        user_input = input()
+        user_input = input("| Please Input Your Number.\n| This Will Reverse It And Find The Difference Between The Two:\n╰-> ")
         valid = validate_input(user_input)
     return user_input
 
 
 def main():
-    ...
+  user_input = get_input()
+  reverse = reversestring(user_input)
+  final = int(user_input) - int(reverse)
+  print(final)
 
 
 if __name__ == "__main__":

a71e2435876b61818980bd996eed961274f57d6c
Evelyne <94754298+RetroEvelyne@users.noreply.github.com>
2022-11-17 15:03:13 +0000

    Luhn Algorithm

--- Messing Around/Algorithms/Luhn Algorithm/main.py
+++ Messing Around/Algorithms/Luhn Algorithm/main.py
@@ -1,12 +1,30 @@
-def reverse_payload(payload: int) -> int:
-    ...
+def luhn_algorithm(payload: str) -> bool:
+    final = 0
+    is_second = False
+    for i in payload:
+        x = int(i)
+        if is_second:
+            x *= 2
+            if x > 9:
+                temp = 0
+                for digit in str(x):
+                    temp += int(digit)
+                x = temp
+        is_second = not is_second
+        final += x
+
+    if final % 10 == 0:
+        return True
+    else:
+        return False
 
 
 def main():
     number = input("Enter a valid number that works with the Luhn algorithm\n > ")
-    checkdigit = int(number[-1])
-    payload = int(number[:-1])
-
+    if luhn_algorithm(number):
+        print("Valid")
+    else:
+        print("Invalid")
 
 
 if __name__ == "__main__":
